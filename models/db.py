39a77a3860be41e4e415212b09c0c660d53c7666
hamid12345 <hami.sadeghi.engineering@gmail.com>
2025-09-15 15:32:28 +0330

    db update

--- models/db.py
+++ models/db.py
@@ -1,6 +1,11 @@
 import sqlite3
+import os
+
+os.makedirs("db", exist_ok=True)
+
 
 def get_connection():
     conn = sqlite3.connect("db/dapp.db", check_same_thread=False)
     conn.execute("PRAGMA foreign_keys = ON")
     return conn
+
